6f5cfb3b7be5ce1491a5c979ffbcb2bf63e6d00a
Vedant Bhagat <vedantbhagat108@gmail.com>
2026-08-22 13:35:40 +0530

    Add files via upload

--- api/auth/change-password.ts
+++ api/auth/change-password.ts
@@ -1,4 +1,4 @@
-import { changeOwnerPassword } from '../server/db.js';
+import { changeOwnerPassword } from '../../server/db.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
--- api/auth/verify-owner.ts
+++ api/auth/verify-owner.ts
@@ -1,4 +1,4 @@
-import { verifyOwnerPassword } from '../server/db.js';
+import { verifyOwnerPassword } from '../../server/db.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
